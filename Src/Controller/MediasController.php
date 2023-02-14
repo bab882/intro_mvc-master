61e1228e70ace3df1b0aca779bb1d97f52dbd2e9
bab882 <chanitebrandon@gmail.com>
2023-02-14 14:27:48 +0100

    first commit

--- Src/Controller/MediasController.php
+++ Src/Controller/MediasController.php
@@ -2,14 +2,37 @@
 
 class MediasController extends Controller
 {
-    function admin_index()
+    function admin_index($id)
     {
         // On a besoin de charger notre méthode
         $this->loadModel('Media');
         if($this->request->data && !empty($_FILES['file']['name']))
         {
-            // On ajoute un layout = modal.php
+            if(strpos($_FILES['file']['type'], 'image' !== false))
+            {
+                $dir = __WEBROOT__ . __DS__ . 'img' . __DS__ . date('Y-m');
+
+                if(!file_exists($dir))
+                {
+                    // Création d'un dossier, de modifier un fichier ou effacer( RW++)
+                    mkdir($dir,  0777);
+                }
+                move_uploaded_file($_FILES['file']['tmp_name'], $dir . __DS__ . $_FILES['file']['name']);
+                $this->Media->save(array(
+                    // On va avoir 4 champs obligatoire
+                    'name' => $this->request->data->name,
+                    'file'  =>dat('Y-m') . '/' . $_FILES['file']['name'],
+                    'post_id' => $id,
+                    'type' => 'img'
+                ));
+                $this->Session->setFlash('L\'image à bien été upbloader');
+            }
+            else
+            {
+                $this->Form->errors['file'] = 'Le fichier n\'est pas une image';
+            }
         }
+        // On ajoute un layout = modal.php
         $this->layout = 'modal';
         //debug($this->request);
     }
